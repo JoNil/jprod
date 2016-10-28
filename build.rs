6ec8faf4a8c3f2f62bea20aa53daeba2b598b4e4
Jonathan Nilsson <lord.jonil@gmail.com>
2016-10-29 00:27:05 +0200

    Stick to 64 bit

--- build.rs
+++
@@ -1,9 +0,0 @@
-extern crate gcc;
-
-fn main() {
-	#[cfg(all(windows, target_pointer_width = "32"))]
-    gcc::Config::new()
-            .object("rt/win32_crt_math.o")
-            .object("rt/mulodi4.o")
-            .compile("librt.a");
-}
